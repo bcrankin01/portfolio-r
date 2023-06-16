75a7c7e4d1831115a65fe2aac44d75fdd728dd82
Ben Rankin <bcrankin01@gmail.com>
2023-06-16 18:21:41 -0400

    created about section and fixed landing styles

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+        <div className="about-wrapper">
+            <div className="about-title">
+                <h3>ABOUT ME</h3>
+            </div>
+            
+        </div>
+    </>
+  )
+}
+
+export default About
